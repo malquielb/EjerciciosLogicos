160e14ab55d8b3b7df17c31c3dba96d3b4682ac7
Malquiel B. H <malquielbeltreh@hotmail.com>
2022-05-22 13:52:30 -0400

    First Commit

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,70 @@
+﻿var runnig = true;
+
+while (runnig) {
+    try
+    {
+        Console.WriteLine("Enter an integer value: ");
+        var input = Console.ReadLine();
+
+        if (input == null)
+            throw new Exception("Invalid input.");
+
+        var arrayLength = int.Parse(input);
+
+        if (arrayLength < 0)
+            throw new Exception("Must enter a positive value.");
+
+        var randomInts = new int[arrayLength];
+        var randomGenerator = new Random();
+
+        for (int i = 0; i < randomInts.Length; i++)
+            randomInts[i] = randomGenerator.Next(1, 11); // random integers in the interval 1-10
+
+        var total = checked(randomInts.Aggregate((a, b) => a * b));
+
+        var result = ComputeResult(total);
+
+        Console.WriteLine("Array: [{0}]", String.Join(",", randomInts.Select(i => i.ToString())));
+        Console.WriteLine("Total: {0}", total);
+        Console.WriteLine("Result: {0}", result);
+
+        Exit();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+int ComputeResult(int total)
+{
+    if (total % 2 == 0)
+        return total;
+
+    return 0;
+}
+
+void Exit()
+{
+    Start:
+    {
+        Console.Write("Exit (y/n): ");
+        var response = Console.ReadLine();
+
+        if (response == null)
+            throw new Exception("Invalid input.");
+
+        switch (response)
+        {
+            case "y":
+                runnig = false;
+                break;
+            case "n":
+                break;
+            default:
+                Console.WriteLine("{0} keyword is not valid.", response);
+                goto Start;
+        }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+var runnig = true;
+
+while (runnig)
+{
+    try
+    {
+        Console.Write("Enter your PIN: ");
+        var pin = Console.ReadLine();
+
+        if (pin == null)
+            throw new Exception("[!!] Invalid input.");
+
+        var result = ValidatePin(pin);
+
+        Console.WriteLine(result);
+
+        Exit();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+string ValidatePin(string pin)
+{
+    var matches = Regex.Matches(pin, @"\d");
+
+    if (matches.Count == 0)
+        return "[X] PIN is invalid (has not digits)";
+
+    if (matches.Count != 4 && matches.Count != 6)
+        return "[X] PIN is invalid (must have length of 4 or 6)";
+
+    var digits = matches.Select(m => m.Value);
+
+    if (digits.Count() != digits.Distinct().Count())
+        return "[X] PIN is invalid (repeated digits)";
+
+    return "[OK] PIN is valid";
+}
+
+void Exit()
+{
+    Start:
+    {
+        Console.Write("Exit (y/n): ");
+        var response = Console.ReadLine();
+
+        if (response == null)
+            throw new Exception("[!!] Invalid input.");
+
+        switch (response)
+        {
+            case "y":
+                runnig = false;
+                break;
+            case "n":
+                break;
+            default:
+                Console.WriteLine("{0} keyword is not valid.", response);
+                goto Start;
+        }
+    }
+}
+
